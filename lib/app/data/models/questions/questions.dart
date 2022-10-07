bac8946a10aae74d90bf9a0e057ff5f06674d977
mirzashowvik <showvikmirza@gmail.com>
2022-10-07 22:38:54 +0600

    work on freeze

--- lib/app/data/models/questions/questions.dart
+++ lib/app/data/models/questions/questions.dart
@@ -1,51 +1,51 @@
-// // To parse this JSON data, do
-// //
-// //     final questions = questionsFromJson(jsonString);
+// To parse this JSON data, do
+//
+//     final questions = questionsFromJson(jsonString);
 
-// import 'package:freezed_annotation/freezed_annotation.dart';
-// import 'dart:convert';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'dart:convert';
 
-// part 'questions.freezed.dart';
-// part 'questions.g.dart';
+part 'questions.freezed.dart';
+part 'questions.g.dart';
 
-// Questions questionsFromJson(String str) => Questions.fromJson(json.decode(str));
+Questions questionsFromJson(String str) => Questions.fromJson(json.decode(str));
 
-// String questionsToJson(Questions data) => json.encode(data.toJson());
+String questionsToJson(Questions data) => json.encode(data.toJson());
 
-// @freezed
-// abstract class Questions with _$Questions {
-//   const factory Questions({
-//     @Default([]) List<Question> questions,
-//   }) = _Questions;
+@freezed
+abstract class Questions with _$Questions {
+  const factory Questions({
+    @Default([]) List<Question> questions,
+  }) = _Questions;
 
-//   factory Questions.fromJson(Map<String, dynamic> json) =>
-//       _$QuestionsFromJson(json);
-// }
+  factory Questions.fromJson(Map<String, dynamic> json) =>
+      _$QuestionsFromJson(json);
+}
 
-// @freezed
-// abstract class Question with _$Question {
-//   const factory Question({
-//     @Default('UnKnown') String question,
-//     @Default(Answers(a: 'UnKnown', b: 'UnKnown', c: 'UnKnown', d: 'UnKnown'))
-//         Answers answers,
-//     @Default('UnKnown') String questionImageUrl,
-//     @Default('UnKnown') String correctAnswer,
-//     @Default(0) num score,
-//   }) = _Question;
+@freezed
+abstract class Question with _$Question {
+  const factory Question({
+    @Default('UnKnown') String question,
+    @Default(Answers(a: 'UnKnown', b: 'UnKnown', c: 'UnKnown', d: 'UnKnown'))
+        Answers answers,
+    @Default('UnKnown') String questionImageUrl,
+    @Default('UnKnown') String correctAnswer,
+    @Default(0) num score,
+  }) = _Question;
 
-//   factory Question.fromJson(Map<String, dynamic> json) =>
-//       _$QuestionFromJson(json);
-// }
+  factory Question.fromJson(Map<String, dynamic> json) =>
+      _$QuestionFromJson(json);
+}
 
-// @freezed
-// abstract class Answers with _$Answers {
-//   const factory Answers({
-//     @Default('UnKnown') String a,
-//     @Default('UnKnown') String b,
-//     @Default('UnKnown') String c,
-//     @Default('UnKnown') String d,
-//   }) = _Answers;
+@freezed
+abstract class Answers with _$Answers {
+  const factory Answers({
+    @Default('UnKnown') String a,
+    @Default('UnKnown') String b,
+    @Default('UnKnown') String c,
+    @Default('UnKnown') String d,
+  }) = _Answers;
 
-//   factory Answers.fromJson(Map<String, dynamic> json) =>
-//       _$AnswersFromJson(json);
-// }
+  factory Answers.fromJson(Map<String, dynamic> json) =>
+      _$AnswersFromJson(json);
+}
--- lib/app/data/models/questions/questions.g.dart
+++ lib/app/data/models/questions/questions.g.dart
@@ -1,54 +1,54 @@
-// // GENERATED CODE - DO NOT MODIFY BY HAND
+// GENERATED CODE - DO NOT MODIFY BY HAND
 
-// part of 'questions.dart';
+part of 'questions.dart';
 
-// // **************************************************************************
-// // JsonSerializableGenerator
-// // **************************************************************************
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
 
-// _$_Questions _$$_QuestionsFromJson(Map<String, dynamic> json) => _$_Questions(
-//       questions: (json['questions'] as List<dynamic>?)
-//               ?.map((e) => Question.fromJson(e as Map<String, dynamic>))
-//               .toList() ??
-//           const [],
-//     );
+_$_Questions _$$_QuestionsFromJson(Map<String, dynamic> json) => _$_Questions(
+      questions: (json['questions'] as List<dynamic>?)
+              ?.map((e) => Question.fromJson(e as Map<String, dynamic>))
+              .toList() ??
+          const [],
+    );
 
-// Map<String, dynamic> _$$_QuestionsToJson(_$_Questions instance) =>
-//     <String, dynamic>{
-//       'questions': instance.questions,
-//     };
+Map<String, dynamic> _$$_QuestionsToJson(_$_Questions instance) =>
+    <String, dynamic>{
+      'questions': instance.questions,
+    };
 
-// _$_Question _$$_QuestionFromJson(Map<String, dynamic> json) => _$_Question(
-//       question: json['question'] as String? ?? 'UnKnown',
-//       answers: json['answers'] == null
-//           ? const Answers(
-//               a: 'UnKnown', b: 'UnKnown', c: 'UnKnown', d: 'UnKnown')
-//           : Answers.fromJson(json['answers'] as Map<String, dynamic>),
-//       questionImageUrl: json['questionImageUrl'] as String? ?? 'UnKnown',
-//       correctAnswer: json['correctAnswer'] as String? ?? 'UnKnown',
-//       score: json['score'] as num? ?? 0,
-//     );
+_$_Question _$$_QuestionFromJson(Map<String, dynamic> json) => _$_Question(
+      question: json['question'] as String? ?? 'UnKnown',
+      answers: json['answers'] == null
+          ? const Answers(
+              a: 'UnKnown', b: 'UnKnown', c: 'UnKnown', d: 'UnKnown')
+          : Answers.fromJson(json['answers'] as Map<String, dynamic>),
+      questionImageUrl: json['questionImageUrl'] as String? ?? 'UnKnown',
+      correctAnswer: json['correctAnswer'] as String? ?? 'UnKnown',
+      score: json['score'] as num? ?? 0,
+    );
 
-// Map<String, dynamic> _$$_QuestionToJson(_$_Question instance) =>
-//     <String, dynamic>{
-//       'question': instance.question,
-//       'answers': instance.answers,
-//       'questionImageUrl': instance.questionImageUrl,
-//       'correctAnswer': instance.correctAnswer,
-//       'score': instance.score,
-//     };
+Map<String, dynamic> _$$_QuestionToJson(_$_Question instance) =>
+    <String, dynamic>{
+      'question': instance.question,
+      'answers': instance.answers,
+      'questionImageUrl': instance.questionImageUrl,
+      'correctAnswer': instance.correctAnswer,
+      'score': instance.score,
+    };
 
-// _$_Answers _$$_AnswersFromJson(Map<String, dynamic> json) => _$_Answers(
-//       a: json['A'] as String? ?? 'UnKnown',
-//       b: json['B'] as String? ?? 'UnKnown',
-//       c: json['C'] as String? ?? 'UnKnown',
-//       d: json['D'] as String? ?? 'UnKnown',
-//     );
+_$_Answers _$$_AnswersFromJson(Map<String, dynamic> json) => _$_Answers(
+      a: json['A'] as String? ?? 'UnKnown',
+      b: json['B'] as String? ?? 'UnKnown',
+      c: json['C'] as String? ?? 'UnKnown',
+      d: json['D'] as String? ?? 'UnKnown',
+    );
 
-// Map<String, dynamic> _$$_AnswersToJson(_$_Answers instance) =>
-//     <String, dynamic>{
-//       'a': instance.a,
-//       'b': instance.b,
-//       'c': instance.c,
-//       'd': instance.d,
-//     };
+Map<String, dynamic> _$$_AnswersToJson(_$_Answers instance) =>
+    <String, dynamic>{
+      'a': instance.a,
+      'b': instance.b,
+      'c': instance.c,
+      'd': instance.d,
+    };
